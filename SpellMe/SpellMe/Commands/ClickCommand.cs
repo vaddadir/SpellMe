f5dfefeb027723c1932d72b535f81a9511157324
Ravi Vaddadi <vaddadir@gmail.com>
2013-12-09 23:09:02 -0500

    More changes

---
+++ SpellMe/SpellMe/Commands/ClickCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SpellMe.Commands
+{
+    public class ClickCommand : ICommand
+    {
+        private readonly Action<object> _clickEventHandler = null;
+        private readonly String _commandName = String.Empty;
+        public String Name
+        {
+            get { return _commandName; }
+        }
+        public ClickCommand(String commandName, Action<object> clickEventHandler)
+        {
+            _clickEventHandler = clickEventHandler;
+            _commandName = commandName;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            if (_clickEventHandler != null)
+                _clickEventHandler(parameter);
+        }
+    }
+}
--- SpellMe/SpellMe/ViewModels/MainViewModel.cs
+++ SpellMe/SpellMe/ViewModels/MainViewModel.cs
@@ -1,17 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SpellMe.Commands;
+using System;
+using System.ComponentModel;
+using System.Windows.Input;
 
 namespace SpellMe.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
         private MainViewModel()
         {
-            SayItText = "Say It";
+            SayAWordButtonText = "Say the word";
+            UseItInASentenceButtonText = "Use the word in a sentence";
+            CloseButtonText = "Close";
+            SubmitButtonText = "Test Me";
+
+            CloseCommand = new ClickCommand("CloseButton", OnCloseClick);
+            SayAWordCommand = new ClickCommand("SayAWordButton", OnSayAWord);
+            SubmitCommand = new ClickCommand("SubmitButton", OnSubmitClick);
         }
+
+        private void OnSubmitClick(object obj)
+        {
+            Results = "Correct !!!";
+        }
+
+        private void OnSayAWord(object obj)
+        {
+        }
+
+        private void OnCloseClick(object obj)
+        {
+            App.Current.Shutdown();
+        }
+
         private static MainViewModel _instance = null;
 
         public static MainViewModel Instance
@@ -19,6 +40,32 @@ namespace SpellMe.ViewModels
             get { return _instance ?? (_instance = new MainViewModel()); }
         }
 
-        public String SayItText { get; set; }
+        public String SayAWordButtonText { get; set; }
+
+        public String UseItInASentenceButtonText { get; set; }
+
+        public String CloseButtonText { get; set; }
+
+        public ICommand CloseCommand { get; set; }
+
+        public ICommand SayAWordCommand { get; set; }
+
+        public String SubmitButtonText { get; set; }
+
+        public ClickCommand SubmitCommand { get; set; }
+
+        private String _results = String.Empty;
+
+        public String Results
+        {
+            get { return _results; }
+            set
+            {
+                _results = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Results"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
