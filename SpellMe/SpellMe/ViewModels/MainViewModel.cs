22996166ab50320ddf0d4391b1509f6dd30aac80
Ravi Vaddadi <vaddadir@gmail.com>
2013-12-09 21:34:36 -0500

    Base Version

---
+++ SpellMe/SpellMe/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellMe.ViewModels
+{
+    public class MainViewModel
+    {
+        private MainViewModel()
+        {
+            SayItText = "Say It";
+        }
+        private static MainViewModel _instance = null;
+
+        public static MainViewModel Instance
+        {
+            get { return _instance ?? (_instance = new MainViewModel()); }
+        }
+
+        public String SayItText { get; set; }
+    }
+}
